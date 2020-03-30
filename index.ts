8805f6a8aab07369e4b93293dc757bd7bd55e0ca
Paco <fcp1978@hotmail.com>
2020-03-30 11:09:46 +0200

    make it better

---
+++ index.ts
@@ -0,0 +1,60 @@
+import Server from "./clases/server";
+import fileupload from 'express-fileupload';
+import bodyParser from "body-parser";
+import cors from 'cors';
+
+
+import mongoose from "mongoose";
+
+import usuarioRutas from "./rutas/usuario";
+import contactoRutas from "./rutas/contacto";
+import yoRutas from "./rutas/imagenesYo";
+import sobreMiRutas from "./rutas/sobreMi";
+import tecnologiasRutas from "./rutas/tecnologia";
+import noticiasRutas from "./rutas/noticias";
+
+
+const server = new Server();
+
+
+// Body Parser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+// CORS
+server.app.use(cors({ origin: true, credentials: true }));
+
+// Fileupload
+server.app.use(fileupload());
+
+// Rutas
+server.app.use('/usuario', usuarioRutas);
+server.app.use('/contacto', contactoRutas);
+server.app.use('/uploadYo', yoRutas);
+server.app.use('/sobreMi', sobreMiRutas);
+server.app.use('/tenologia', tecnologiasRutas);
+server.app.use('/noticias', noticiasRutas);
+
+// Conectar Base de Datos
+
+let mongoDB: string;
+
+if (process.env.NODE_ENV === 'production') {
+    mongoDB = 'mongodb+srv://moro:OR5BRnAXedqikZg8@cluster0-d6hzk.mongodb.net/FedeDJBase'
+} else {
+    mongoDB = 'mongodb://localhost:27017/FedeDJBase'
+}
+mongoose.connect(
+    mongoDB,
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false },
+    (err) => {
+        if (err) throw "err";
+        console.log('Base de datos ONLINE');
+    }
+)
+
+
+// Levantar el servidor
+server.start(() => {
+    console.log(`Servidor FEDE corriendo el el puerto ${server.port}`);
+})
